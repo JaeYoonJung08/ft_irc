1ee45966e0ddc54fced4ad60904fa2e3090bd17a
JaeYoonJung08 <whiltecy01@naver.com>
2023-10-30 22:11:07 +0900

    10_30_fin

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,28 +1,56 @@
 #include "../inc/Channel.hpp"
 
-
 Channel::Channel() {}
 
 Channel::~Channel() {}
-
 
 Channel::Channel(const std::string &name) : name(name)
 {
     this->topicTitle = "";
 }
 
-const std::string &Channel::getName() const {
+const std::string &Channel::getName() const 
+{
     return name;
 }
 
-const std::string &Channel::getTopicTitle() const {
+const std::string &Channel::getTopicTitle() const 
+{
     return topicTitle;
 }
 
-const std::list<std::string> &Channel::getMembers() const {
+const  std::map<std::string, int>  &Channel::getMembers() const 
+{
     return members;
 }
 
-const std::list<std::string> &Channel::getOperators() const {
-    return operators;
+void Channel::topic(const std::string &newtopicTitle) 
+{
+    this->topicTitle = newtopicTitle;
+}
+
+void Channel::mode()
+{
+
+}
+
+int Channel::kick(const std::string &nickname)
+{
+    std::map<std::string, int>::iterator iter = members.find(nickname);
+    
+    if (iter == members.end()) // 유저 존재 x
+    {
+        std::cout << "no nickname" << std::endl;
+        return 0;
+    }
+    else
+    {
+        members.erase(nickname);
+        // TODO : kick 되었다고 전송?
+    }
+}
+
+void Channel::invite(const std::string &nickname)
+{
+    members[nickname] = 0; // 기본 멤버로 초대
 }
