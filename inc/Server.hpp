911f188cc94bab3bbec92c93f008115da442ba94
JaeYoonJung08 <whiltecy01@naver.com>
2023-11-03 23:40:13 +0900

    singletion

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -18,6 +18,7 @@
 #include "../inc/Channel.hpp"
 #include "../inc/Client.hpp"
 #include "../inc/Message.hpp"
+#include "../inc/Command.hpp"
 
 #define LISTEN_BACKLOG_NUM 5 // listen 에서 대기 큐 갯수.
 
@@ -56,6 +57,18 @@ class Server
     Server(int portNumber, std::string password);
     ~Server();
 
+/////////////////////////////////////////
+    // Command 클래스에 Server 인스턴스를 제공하는 메서드 추가
+    Command& getCommandInstance();
+    
+    // 새로운 함수를 통해 Server 클래스의 멤버 변수에 대한 액세스 제공
+    //int getPortNumber() const { return portNumber;}
+    const std::map<std::string, int>& getNicknameToSocketFd() const 
+    {
+        return nicknameToSocketFd;
+    }
+/////////////////////////////////
+
     void openSocket();
     void init();
     void run();
@@ -67,44 +80,13 @@ class Server
     void execCommand(Message message);
     bool password_checker(const std::string &str);
 
-    /* clients */
-    void pass(Message &message);
-    void nick(Message &message);
-    void user(Message &message);
-    void privmsg(Message &message);
-    void ping(Message &message);
-    void pong(Message &message);
-    void join(Message &message);
-    int joinChannelNameCheck(std::string name);
-    void part(Message &message);   // 채널 퇴장
-    void quit(std::string reason); // 서버 접속 끊기
-    void exit(void);               // 서버 접속 끊고 프로그램 종료
 
-    /* operators */
-    void kick(Message &message);
-    void topic(Message &message);
-    void invite(Message &message);
-    void mode(Message &message);
-
-    /* command utils */
-    bool setMode(Message &message, Channel channel);
-
-    /* error */
-    void state_without_setup_324(Message &message);
-    void password_incorrect_464(Message &message);
-    void command_empty_argument_461(Message &message);
-
-    /* nick 433, 431 */
-    void	nick_duplicate_check_433(Message &message);
-    void    nick_empty_argument_431(Message &message);
-
-    /* kick 403 */
-    void no_such_channel_403(Message &message);
-    void no_member_channel_442(Message &message);
-    void user_already_channel_443(Message &message);
-    void no_operator_channel_482(Message &message);
-    void kick_no_users_channel_441(Message &message);
 
 };
-
+/////////////////////
+// Command& Server::getCommandInstance()
+//  {
+//     return Command::getInstance(*this); // Command 클래스 생성 시 Server 클래스 인스턴스 전달
+// }
+///////////////////////
 #endif
